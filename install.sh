bb5df1c0d6c8d42fb8952c157d2c330ed7d6e1dc
Matthew Rubino <83928138+mattrrubino@users.noreply.github.com>
2024-03-09 19:54:32 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 git clone --bare git@github.com:mattrrubino/.dotfiles.git $HOME/.dotfiles
-alias config='/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
+function config {
+        /usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME $@
+}
 config config --local status.showUntrackedFiles no
 config checkout
 
 # Handle checkout failure
-if [ $? != 0 ]; then
+if [ $? != 0 ]
+then
   mkdir -p .config-backup
   config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
   config checkout
