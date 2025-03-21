0c53f4863f3da8b496c95c67d87c0f1cc81dd103
Matthew Rubino <mrrubino@buffalo.edu>
2025-03-20 21:45:27 -0400

    Add copilot

--- .config/nvim/lua/mattrrubino/keymaps.lua
+++ .config/nvim/lua/mattrrubino/keymaps.lua
@@ -5,24 +5,28 @@ k.set("n", "<Leader>f", ":Telescope find_files<CR>")
 
 -- LSP
 vim.api.nvim_create_autocmd('LspAttach', {
-    callback = function(args)
-        local client = vim.lsp.get_client_by_id(args.data.client_id)
-        if client.server_capabilities.hoverProvider then
-            k.set('n', '<C-k>', vim.lsp.buf.hover, { buffer = args.buf })
-            vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
-        end
-    end,
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client.server_capabilities.hoverProvider then
+      k.set('n', '<C-k>', vim.lsp.buf.hover, { buffer = args.buf })
+      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+    end
+  end,
 })
+
+-- Copilot
+k.set("i", '<S-Space>', 'copilot#Accept("")', { expr = true, replace_keycodes = false })
+vim.g.copilot_no_tab_map = true
 
 -- DAP
 k.set("n", "<Leader>b", ":DapToggleBreakpoint<CR>")
 k.set("n", "<Leader>r", function()
-    require("dapui").open()
-    vim.cmd("DapContinue")
+  require("dapui").open()
+  vim.cmd("DapContinue")
 end)
 k.set("n", "<Leader>e", function()
-    require("dapui").close()
-    vim.cmd("DapTerminate")
+  require("dapui").close()
+  vim.cmd("DapTerminate")
 end)
 k.set("n", "<F8>", ":DapStepInto<CR>")
 k.set("n", "<F9>", ":DapStepOver<CR>")
